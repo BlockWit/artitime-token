4ac0984101d57535fa4cd5d67fed13c3de1749b9
Arkadiy A.Davydov <jah.knows.me.well@gmail.com>
2021-12-04 17:07:47 +0300

    test(token): add liquidity tests

--- test/token.uniswap.test.js
+++ test/token.uniswap.test.js
@@ -1,6 +1,7 @@
 const { accounts, contract } = require('@openzeppelin/test-environment')
-const { BN, constants, ether, time } = require('@openzeppelin/test-helpers');
-const {fromArtifact, getEvents} = require("./util");
+const { BN, ether, time } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const { fromArtifact } = require("./util");
 
 const ArtiTimeToken = contract.fromArtifact('ArtiTimeToken');
 const WETH = contract.fromArtifact('canonical-weth/WETH9');
@@ -8,7 +9,7 @@ const UniswapFactory = fromArtifact("@uniswap/v2-core/build/UniswapV2Factory.jso
 const UniswapRouter = fromArtifact("@uniswap/v2-periphery/build/UniswapV2Router02.json");
 const UniswapPairMock = fromArtifact("@uniswap/v2-core/build/UniswapV2Pair.json");
 
-const [uniswapOwner, owner] = accounts;
+const [uniswapOwner, owner, account1, account2, developers, marketing] = accounts;
 
 const FEES = {
   BURN: 1,
@@ -40,11 +41,61 @@ describe('Token', async function () {
     const deadline = (await time.latest()).addn(300);
     await token.approve(uniswapRouter.address, liquidity, {from: owner});
     await uniswapRouter.addLiquidityETH(token.address, liquidity, liquidity, value, owner, deadline, {from: owner, value})
+    // configure token
+    await token.setLiquidityFeePercent(FEES.LIQUDITY, {from: owner});
+    await token.setUniswapRouter(uniswapRouter.address, {from: owner});
+    await token.setUniswapPair(uniswapPair.address, {from: owner});
+    await token.toggleSwapAndLiquify({from: owner});
   });
 
-  it('yolo', async function() {
-    console.log((await uniswapPair.totalSupply()).toString());
-  });
+  describe('transfer', () => {
+    it('should charge correct liquidity fee', async function() {
+      const value = ether('12345');
+      await token.transfer(account1, value, {from: owner});
+      const account1Balance = await token.balanceOf(account1);
+      const tokenBalance = await token.balanceOf(token.address);
+      expect(tokenBalance).to.be.bignumber.equal(value.mul(new BN(FEES.LIQUDITY)).div(new BN(100)));
+      expect(tokenBalance.add(account1Balance)).to.be.bignumber.equal(value);
+    });
+
+    it('should not trigger replenishment of the liquidity pool when token balance is below the max value', async function() {
+      const liquidityThreshhold = await token.liquidityThreshold();
+      const tokensToSend = liquidityThreshhold.mul(new BN(100)).div(new BN(FEES.LIQUDITY));
+      await token.transfer(account1, tokensToSend, {from: owner});
+      const tokenBalance = await token.balanceOf(token.address);
+      expect(tokenBalance).to.be.bignumber.equal(liquidityThreshhold);
+    });
+
+    it('should trigger replenishment of the liquidity pool when token balance exceeds the max value', async function() {
+      const liquidityThreshhold = await token.liquidityThreshold();
+      const tokensToSend = liquidityThreshhold.mul(new BN(100)).div(new BN(FEES.LIQUDITY));
+      await token.transfer(account1, tokensToSend, {from: owner});
+      const tokenBalanceBeforeSwap = await token.balanceOf(token.address);
+      await token.transfer(account1, new BN(1), {from: owner});
+      const tokenBalanceAfterSwap = await token.balanceOf(token.address);
+      expect(tokenBalanceBeforeSwap).to.be.bignumber.equal(liquidityThreshhold);
+      expect(tokenBalanceAfterSwap).to.be.zero;
+    });
+
+    it('should trigger replenishment of the liquidity pool correctly when all fees are enabled', async function() {
+      await Promise.all([
+        token.setTaxFeePercent(FEES.RFI, {from: owner}),
+        token.setBurnFeePercent(FEES.BURN, {from: owner}),
+        token.setDevelopersRewardPercent(FEES.DEVELOPERS, {from: owner}),
+        token.setMarketingRewardPercent(FEES.MARKETING, {from: owner}),
+        token.setDevelopersAddress(developers, {from: owner}),
+        token.setMarketingAddress(marketing, {from: owner})
+      ]);
+      const liquidityThreshhold = await token.liquidityThreshold();
+      const tokensToSend = liquidityThreshhold.mul(new BN(100)).div(new BN(FEES.LIQUDITY));
+      await token.transfer(account1, tokensToSend, {from: owner});
+      const tokenBalanceBeforeSwap = await token.balanceOf(token.address);
+      await token.transfer(account1, new BN(1), {from: owner});
+      const tokenBalanceAfterSwap = await token.balanceOf(token.address);
+      expect(tokenBalanceBeforeSwap).to.be.bignumber.gte(liquidityThreshhold);
+      expect(tokenBalanceAfterSwap).to.be.zero;
+    });
+  })
 
 });
 
